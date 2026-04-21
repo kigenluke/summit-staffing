753b093fadce8e811d7aac4a710e71ebdf0ed108
basimtanveer <basimjutt2017@gmail.com>
2026-04-21 12:50:28 +0500

    stripe code added into git

--- app.backend.js
+++ app.backend.js
@@ -26,6 +26,9 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:5175',
+  'http://localhost:5176',
   'http://localhost:19006',
   'https://athletic-heart-backend-production.up.railway.app',
 ];
